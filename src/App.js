f08c12f339e7eb133f60caa176308e4ae37b1068
KyeongHoSeong <dasomaru@gmail.com>
2020-09-07 06:31:54 +0900

    003: props로 state, function 전달

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Person from './components/Person';
-import Animal from './components/Animal';
 
 class App extends Component {
   constructor(){
@@ -12,57 +11,32 @@ class App extends Component {
         { name: "홍길동", age: 20 },
         { name: "이흥부", age: 33 },
         { name: "박놀부", age: 43 },
-      ],
-      animal: [
-        { name: "펭귄", area:"남극"},
-        { name: "코끼리", area:"아프리카"},
       ]
     };
   }
 
   render() {
-    const {person, animal} = this.state;
+    const {person} = this.state;
+    const myFunction = () => { console.log("I am function")}
     return (
       <div className="App">
-        {/* Task: 여기에 특정 내용(사람의 이름과 나이)를  표시.*/}
-        {/* component를 통하여 과제를 해결하도록 만든다. */}
-        {/* 속성으로 component에 값을 전달할 수 있다. */}
-        {/* <Person 
-          name={this.state.person[0].name} 
-          age={this.state.person[0].age}>
-          {" "}
-        </Person>
-        <Person
-          name={this.state.person[1].name}
-          age={this.state.person[1].age}
-        />
-        <Person
-          name={this.state.person[2].name}
-          age={this.state.person[2].age}
-        /> */}
-
         <Person 
-          name={person[0].name} 
-          age={person[0].age}>
-          {" "}
-        </Person>
-        <Person
-          name={person[1].name}
-          age={person[1].age}
-        />
-        <Person
-          name={person[2].name}
-          age={person[2].age}
-        />
-        {/* <Animal name={"원숭이"} area={"아프리카"} /> */}
-        {/* <Animal name={this.state.animal[0].name} area={this.state.animal[0].area} />
-        <Animal name={this.state.animal[1].name} area={this.state.animal[1].area} /> */}
+            name={person[0].name} 
+            age={person[0].age}
+            //myFunction = { () => console.log("I am function")}
+            myFunction = {myFunction}
 
-        <Animal name={animal[0].name} area={animal[0].area} />
-        <Animal name={animal[0].name} area={animal[1].area} />
-
+            // object 자체를 보내기
+            stat = {this.state}
+        />
       </div>
-    );
+        //     {/* function을 props로 전달하기  */}
+        //     {/* myFunction ={ () => console.log("test");}은 myFunction ={ () => return console.log("test")} */}
+        // {/* children 전달 */}
+        //     {/* <div>I am Children!!! (of person)</div> */}
+        // {/* </Person> */}
+      
+    )
   }
 }
 
