6c62a5773339ca848f86f2cb958d5ce15bc1674f
KyeongHoSeong <dasomaru@gmail.com>
2020-09-07 06:35:53 +0900

    props function

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,29 +1,16 @@
-// task:  이름과 나이를 전달 받아 페이지 렌드링
-// props를 통하여 부모로부터 값을 전달 받는다.
-// jsx내에서 자바스크립트를 표현하는 방법은 {}
-import React, { Component } from 'react'
-// // arrow function
-//const Person = (props) => (
-// const Person = ({ name, age }) => (
-//   <div>
-//     {/* 이름: {props.name}, 나이: {props.age} */}
-//     이름: {name}, 나이: {age}
-//   </div>
-// );
+import React from 'react'
 
-// class component
-class Person extends Component {
-    render() {
-        const { name, age} = this.props;
-        return (
+const Person = (props) => (
             <div>
-                {/* 이름: {this.props.name}, 나이: {this.props.age} */}
-                이름: {name}, 나이: {age}
-            </div>
-        )
-    }
-}
+                <h1> 이름: {props.name}, 나이: {props.age} </h1>
+                <h2> Who are you? {props.children}  </h2>
+                {/* {props.myFunction} 함수 가져오기*/}
+                {/* 함수를 실행하려면... */}
+                {props.myFunction()} 
+                { console.log(props.stat)}
 
+            </div>
+)
 export default Person
 
  
